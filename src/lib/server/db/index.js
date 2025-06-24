691147b89b3279d5c0810b2784c7b9b7015ef155
Michael Durbin <mike@midnightviking.com>
2025-06-24 07:17:53 -0400

    s

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -5,7 +5,7 @@ import { env } from '$env/dynamic/private';
 const isProduction = env.NODE_ENV === 'production';
 const databaseUrl = isProduction ? env.DB : env.DATABASE_URL;
 if (!databaseUrl) throw new Error('Database URL is not set');
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = createClient({ url: env.DATABASE_URL });
 
